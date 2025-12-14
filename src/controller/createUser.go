95ba471f62623d050d6704016799d1946b445532
Tommy <thomasjefferson.dev@gmail.com>
2025-12-14 19:46:46 -0300

    feat: validation user implemented

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"fmt"
-
-	"github.com/Tommy-jeff/first_go_crud/src/configs/rest_err"
+	"github.com/Tommy-jeff/first_go_crud/src/configs/validation"
 	"github.com/Tommy-jeff/first_go_crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -13,8 +11,7 @@ func CreateUser(c *gin.Context) {
 	var UserRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&UserRequest); err != nil {
-		restErr := resterr.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect fields, error=%v", err.Error()))
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 	}
 
