00bf381a4e233b05fc61af1803a75c99078228a2
Tommy <thomasjefferson.dev@gmail.com>
2025-12-26 22:05:05 -0300

    feat: zap logger implemented and applied in createUser controller

--- src/configs/logger/logger.go
+++ src/configs/logger/logger.go
@@ -35,6 +35,16 @@ func init() {
 
 }
 
+func Info (message string, tags ...zap.Field) {
+	log.Info(message, tags...)
+	log.Sync()
+}
+func Error (message string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.Info(message, tags...)
+	log.Sync()
+}
+
 func getOutoputLogs() string {
 	output := strings.ToLower(strings.TrimSpace(os.Getenv(LOG_OUTPUT)))
 	if output == "" {
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,19 +1,52 @@
 package controller
 
 import (
+	"net/http"
+
+	"github.com/Tommy-jeff/first_go_crud/src/configs/logger"
 	"github.com/Tommy-jeff/first_go_crud/src/configs/validation"
 	"github.com/Tommy-jeff/first_go_crud/src/controller/model/request"
+	"github.com/Tommy-jeff/first_go_crud/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap/zapcore"
 )
 
-/// Função que cria um user
+// / Função que cria um user
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser Controller",
+		zapcore.Field{
+			Key:    "journey",
+			String: "createUser",
+		},
+	)
 
 	var UserRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&UserRequest); err != nil {
+		logger.Error("Error trying to validade user info", err,
+			zapcore.Field{
+				Key:    "journey",
+				String: "createUser",
+			},
+		)
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
+		return
 	}
 
+	response := response.UserResponse{
+		ID:    1,
+		Name:  UserRequest.Name,
+		Email: UserRequest.Email,
+		Age:   UserRequest.Age,
+	}
+
+	logger.Info("user created succesfully",
+		zapcore.Field{
+			Key:    "journey",
+			String: "createUser",
+		},
+	)
+
+	c.JSON(http.StatusOK, response)
 }
